7e934663ff5bfdbbfdf6d0dff58a1d6f8fedb3e8
Daniel Kent <Kentpercussion@gmail.com>
2019-11-01 14:18:22 -0600

    started fetch

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const apiKey = 'MghaYsUpHaHrgblfI7IcgQgucoKbB3gUMzY6HR7e';
-const baseUrl = 'developer.nps.gov/api/v1';
+const baseUrl = 'https://developer.nps.gov/api/v1';
 
+function getParks () {
+  const options = {
+    headers: new Headers({
+      'X-Api-Key': apiKey
+    })  
+  };
+
+  fetch('https://developer.nps.gov/api/v1/parks?stateCode=tx', options)
+    .then(response => response.json())
+    .then(responseJson => console.log(responseJson));
+
+}
+
+getParks();
