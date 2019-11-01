1fe294166f17a8122548487f7d47a2173b81eb7e
Daniel Kent <Kentpercussion@gmail.com>
2019-11-01 14:35:39 -0600

    removed header

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ const apiKey = 'MghaYsUpHaHrgblfI7IcgQgucoKbB3gUMzY6HR7e';
 const baseUrl = 'https://developer.nps.gov/api/v1';
 
 function getParks () {
-  const options = {
-    headers: new Headers({
-      'X-Api-Key': apiKey
-    })  
-  };
 
-  fetch('https://developer.nps.gov/api/v1/parks?stateCode=tx', options)
+  fetch('https://developer.nps.gov/api/v1/parks?stateCode=tx&api_key=MghaYsUpHaHrgblfI7IcgQgucoKbB3gUMzY6HR7e')
     .then(response => response.json())
     .then(responseJson => console.log(responseJson));
 
