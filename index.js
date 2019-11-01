2ffaf9e953e10a151773ec027c2907bfdf7922d1
Sophia Koeut <koeutsophia@gmail.com>
2019-11-01 14:13:38 -0700

    changes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const apiKey = 'MghaYsUpHaHrgblfI7IcgQgucoKbB3gUMzY6HR7e';
 const baseUrl = 'https://developer.nps.gov/api/v1';
+const arrayStates = ['AL', 'AK', 'AS', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FM', 'FL', 'GA', 'GU', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MH', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'MP', 'OH', 'OK', 'OR', 'PW', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VI', 'VA', 'WA', 'WV', 'WI', 'WY'];
 
-function getParks () {
+function getParks() {
 
   fetch('https://developer.nps.gov/api/v1/parks?stateCode=tx&api_key=MghaYsUpHaHrgblfI7IcgQgucoKbB3gUMzY6HR7e')
     .then(response => response.json())
@@ -9,4 +10,12 @@ function getParks () {
 
 }
 
-getParks();
+function submitHandler() {
+  $('#state-submit').submit(event => {
+    event.preventDefault();
+    getParks();
+  }
+  );
+};
+
+submitHandler();
